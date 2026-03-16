ad87c12d06c96706526a1a30c9ba2b6cdf2c5033
gabinprat-bit <gabinprat@icloud.com>
2026-03-16 19:14:01 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -60,17 +60,14 @@ void main() async {
 class ENTApp extends StatelessWidget {
   const ENTApp({super.key});
   @override
-  Widget build(BuildContext context) => ChangeNotifierProvider(
-    create: (_) => AppState()..init(),
-    child: MaterialApp(
-      title: 'Mon ENT',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF2F80ED), brightness: Brightness.dark),
-        useMaterial3: true,
-        scaffoldBackgroundColor: Colors.transparent,
-      ),
-      home: const RootScreen(),
+  Widget build(BuildContext context) => MaterialApp(
+    title: 'Mon ENT',
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+      colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF2F80ED), brightness: Brightness.dark),
+      useMaterial3: true,
+      scaffoldBackgroundColor: Colors.transparent,
     ),
+    home: const RootScreen(),
   );
 }
